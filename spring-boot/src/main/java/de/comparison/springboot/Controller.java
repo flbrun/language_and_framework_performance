855aa9d45fd54b1f9e5eaa37590c7c7e7550c09b
Florian Brunner <87166260+flbrun@users.noreply.github.com>
2023-07-10 14:50:15 +0200

    Add files via upload

---
+++ spring-boot/src/main/java/de/comparison/springboot/Controller.java
@@ -0,0 +1,39 @@
+package de.comparison.springboot;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/spring-boot")
+public class Controller {
+
+    @GetMapping("/hello-world")
+    ResponseEntity<String> helloWorld()
+    {
+        return ResponseEntity.ok("Hello World !");
+    }
+    @GetMapping("/greeting/{name}")
+    ResponseEntity<String> greeting(@PathVariable String name)
+    {
+        return ResponseEntity.ok("Hello " + name + " !");
+    }
+    @GetMapping("/fibonacci/{length}")
+    ResponseEntity<String> fibonacci(@PathVariable int length) {
+        if (length > 1) {
+            long[] fibonacci = new long[length];
+            fibonacci[0] = 0;
+            fibonacci[1] = 1;
+
+            for (int i = 2; i < length; i++) {
+                fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+            }
+           return ResponseEntity.ok(String.valueOf(fibonacci[fibonacci.length-1]));
+        }
+       return ResponseEntity.ok("Fibonacci calculation not possible !");
+    }
+
+}
