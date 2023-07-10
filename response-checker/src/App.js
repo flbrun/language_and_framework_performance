512b6f5b570f845da7889e284eab8bf8a43beaea
Florian Brunner <florian-home@web.de>
2023-07-10 21:49:40 +0200

    create Server selection

--- response-checker/src/App.js
+++ response-checker/src/App.js
@@ -1,23 +1,66 @@
 import logo from './logo.svg';
 import './App.css';
+import TextField from '@mui/material/TextField';
+import MenuItem from '@mui/material/MenuItem';
+
+const protocoll = [
+    {
+        value: 'http://',
+    },
+    {
+        value: 'https://',
+    },
+    {
+        value: 'ws://',
+    },
+    {
+        value: 'wss://',
+    },
+];
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Response Checker</p>
       </header>
+      <body>
+          <div className="Server-selection">
+              <div className="Server-selection-border">
+                  <h2 className="Server-selection-heading">Server Selection</h2>
+
+                  <TextField
+                      id="outlined-select-currency"
+                      sx={{ m: 1, width: '12ch' }}
+                      select
+                      label="Select"
+                      defaultValue="http"
+                  >
+                      {protocoll.map((option) => (
+                          <MenuItem key={option.value} value={option.value}>
+                              {option.value}
+                          </MenuItem>
+                      ))}
+                  </TextField>
+
+                  <TextField
+                      id="outlined-basic"
+                      sx={{ m: 1 }}
+                      label="Server"
+                      variant="outlined"
+                  />
+
+                  <p className="Colon">:</p>
+
+                  <TextField
+                      id="outlined-basic"
+                      label="Port"
+                      sx={{ m: 1, width: '10ch' }}
+                      variant="outlined"
+                  />
+              </div>
+          </div>
+      </body>
     </div>
   );
 }
