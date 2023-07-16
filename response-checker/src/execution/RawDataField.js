718352eba3e83091d505757612adfb0e84050104
Florian Brunner <florian-home@web.de>
2023-07-16 14:08:32 +0200

    responses as Table

--- response-checker/src/execution/RawDataField.js
+++ response-checker/src/execution/RawDataField.js
@@ -1,46 +1,117 @@
-import React, { useState, useEffect, useCallback } from 'react';
-import { DataGrid } from '@mui/x-data-grid';
-import { Alert, Box } from '@mui/material';
+import React, { useState, useEffect } from 'react';
+import Paper from "@mui/material/Paper";
+import { Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow } from "@mui/material";
 
 const columns = [
-    { field: 'id', headerName: 'ID'},
-    { field: 'statusCode', headerName: 'Status Code'},
-    { field: 'duration', headerName: 'Response Time', width: 300 },
+    { id: 'id', label: 'ID', sortable: true },
+    { id: 'statusCode', label: 'Status Code', sortable: true },
+    { id: 'duration', label: 'Response Time (ms)', sortable: true },
 ];
 
 export const RawDataField = ({ responses }) => {
     const [rows, setRows] = useState([]);
-    const [detailPanelExpandedRowIds, setDetailPanelExpandedRowIds] = useState([]);
+    const [page, setPage] = useState(0);
+    const [rowsPerPage, setRowsPerPage] = useState(10);
+    const [sortField, setSortField] = useState('id');
+    const [sortOrder, setSortOrder] = useState('asc');
+
+    const handleSort = (field) => {
+        const column = columns.find((col) => col.id === field);
+        if (column.sortable) {
+            if (sortField === field) {
+                setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc');
+            } else {
+                setSortField(field);
+                setSortOrder('asc');
+            }
+        }
+    };
 
     useEffect(() => {
         let tempRows = responses.map((response, index) => ({
             id: index + 1,
             statusCode: response.responseStatus,
             duration: response.duration,
-            // headers: response.responseHeaders,
-            // body: response.responseBody,
+            headers: response.responseHeaders,
+            body: response.responseBody,
         }));
 
-        setRows(tempRows);
-    }, [responses]);
+        tempRows.sort((a, b) => {
+            const aValue = a[sortField];
+            const bValue = b[sortField];
+            if (sortOrder === 'asc') {
+                return aValue - bValue;
+            } else {
+                return bValue - aValue;
+            }
+        });
 
-    const handleDetailPanelExpandedRowIdsChange = useCallback((newIds) => {
-        setDetailPanelExpandedRowIds(newIds);
-    }, []);
+        setRows(tempRows);
+    }, [responses, sortField, sortOrder]);
+
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(+event.target.value);
+        setPage(0);
+    };
 
     return (
-        <Box sx={{ width: '450px' }}>
-            <Box sx={{ height: 400, mt: 1 }}>
-                <DataGrid
-                    rows={rows}
-                    columns={columns}
-                    rowThreshold={0}
-                    getDetailPanelContent={({ row }) => <Box sx={{ p: 2 }}>{`Order #${row.id}`}</Box>}
-                    getDetailPanelHeight={() => 50}
-                    detailPanelExpandedRowIds={detailPanelExpandedRowIds}
-                    onDetailPanelExpandedRowIdsChange={handleDetailPanelExpandedRowIdsChange}
-                />
-            </Box>
-        </Box>
+        <Paper sx={{ width: '400px', overflow: 'hidden' }}>
+            <TableContainer sx={{ maxHeight: 440 }}>
+                <Table stickyHeader aria-label="sticky table">
+                    <TableHead>
+                        <TableRow>
+                            {columns.map((column) => (
+                                <TableCell
+                                    key={column.id}
+                                    align={column.align}
+                                    style={{ minWidth: column.minWidth, cursor: column.sortable ? 'pointer' : 'default' }}
+                                    onClick={() => handleSort(column.id)}
+                                >
+                                    {column.label}
+                                    {column.sortable && (
+                                        <span style={{ marginLeft: '4px', display: 'inline-block', transform: sortField === column.id ? `rotate(${sortOrder === 'asc' ? '180deg' : '0deg'})` : 'rotate(0deg)', transition: 'transform 0.2s' }}>
+                      ▼
+                    </span>
+                                    )}
+                                </TableCell>
+                            ))}
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {rows
+                            .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                            .map((row) => {
+                                return (
+                                    <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                                        {columns.map((column) => {
+                                            const value = row[column.id];
+                                            return (
+                                                <TableCell key={column.id} align={column.align}>
+                                                    {column.format && typeof value === 'number'
+                                                        ? column.format(value)
+                                                        : value}
+                                                </TableCell>
+                                            );
+                                        })}
+                                    </TableRow>
+                                );
+                            })}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            <TablePagination
+                rowsPerPageOptions={[10, 25, 100]}
+                component="div"
+                count={rows.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                onPageChange={handleChangePage}
+                onRowsPerPageChange={handleChangeRowsPerPage}
+            />
+        </Paper>
     );
 };
