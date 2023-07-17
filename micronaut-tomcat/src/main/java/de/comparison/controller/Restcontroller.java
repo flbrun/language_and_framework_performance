dff92eec993bf6e2d032fb7d89c08375c105bd62
Florian Brunner <87166260+flbrun@users.noreply.github.com>
2023-07-17 10:55:15 +0200

    Add files via upload

---
+++ micronaut-tomcat/src/main/java/de/comparison/controller/Restcontroller.java
@@ -0,0 +1,51 @@
+package de.comparison.controller;
+
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.HttpStatus;
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.PathVariable;
+import io.micronaut.http.annotation.Produces;
+
+@Controller("/micronaut-tomcat")
+public class Restcontroller {
+
+    @Get("/hello-world")
+    @Produces(MediaType.TEXT_PLAIN)
+    public HttpResponse<String> helloWorld(){
+        return HttpResponse
+                .status(HttpStatus.OK)
+                .body("Hello World !");
+
+    }
+
+    @Get("/greeting/{name}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public HttpResponse<String> greeting(@PathVariable String name){
+        return HttpResponse
+                .status(HttpStatus.OK)
+                .body("Hello " + name + " !");
+
+    }
+
+    @Get("/fibonacci/{length}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public HttpResponse<String> fibonacci(@PathVariable int length){
+        if (length > 1) {
+            long[] fibonacci = new long[length];
+            fibonacci[0] = 0;
+            fibonacci[1] = 1;
+
+            for (int i = 2; i < length; i++) {
+                fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+            }
+            return HttpResponse
+                    .status(HttpStatus.OK)
+                    .body(String.valueOf(fibonacci[fibonacci.length-1]));
+        }
+        return HttpResponse
+                .status(HttpStatus.OK)
+                .body("Fibonacci calculation not possible !");
+    }
+}
