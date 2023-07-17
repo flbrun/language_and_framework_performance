66fd7130be6c6a15c38c1fd6af1e95b27e5a11d4
Florian Brunner <87166260+flbrun@users.noreply.github.com>
2023-07-17 13:03:33 +0200

    Add files via upload

---
+++ spring-boot-netty/src/test/java/de/comparison/springbootnetty/SpringBootNettyApplicationTests.java
@@ -0,0 +1,13 @@
+package de.comparison.springbootnetty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootNettyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
